bd1de9430324d252fd4b11cdb994c63b816c5873
hyejin <totoro2523@hotmail.comgit commit -m test>
2026-05-27 21:04:10 +0900

    feature:260527, page layout todo

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,14 +1,55 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #eeeaea;
+`;
+const Box = styled.div`
+  width: 400px;
+  height: 600px;
+  background: #fff;
+  box-shadow: 0px 25px 100px -60px rgba(0, 0, 0, 0.18);
+`;
+const Title = styled.h1`
+  margin: 0;
+  padding: 15px 25px;
+`;
+const TodoList = styled.div``;
+const TdoItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px 25px;
+  width: 100%;
+  font-size: 1.2em;
+  border-bottom: 1px solid #eee;
+`;
+const TodoCheck = styled.input`
+  margin-right: 15px;
+`;
 export function HomePage() {
   return (
     <>
       <Helmet>
-        <title>HomePage</title>
-        <meta name="description" content="A Boilerplate application homepage" />
+        <title>main</title>
+        <meta name="description" content="todo main app" />
       </Helmet>
-      <span>My HomePage</span>
+      <Wrapper>
+        <Box>
+          <Title>할 일</Title>
+          <TodoList>
+            <TdoItem>
+              <TodoCheck type="checkbox" />
+              나는 투두
+            </TdoItem>
+          </TodoList>
+        </Box>
+      </Wrapper>
     </>
   );
 }
