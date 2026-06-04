a2d465ddf04c12bbd97bafcfb799f025bf5a52d8
hyejin <totoro2523@hotmail.comgit commit -m test>
2026-06-04 19:53:01 +0900

    test

--- src/app/components/TodoInput/index.tsx
+++ src/app/components/TodoInput/index.tsx
@@ -1,9 +1,10 @@
+import * as React from 'react';
 import styled from 'styled-components';
 
 const Box = styled.div`
   display: flex;
   align-items: center;
-  width: 100%auto;
+  width: 100%;
   padding: 15px 25px;
   font-size: 1.1em;
   border-bottom: 1px solid #eee;
@@ -16,10 +17,30 @@ const Input = styled.input`
   font-size: 1.1em;
 `;
 
-export default function TodoInput() {
+interface Props {
+  setTodoList: (todo: ITodoItem) => void;
+}
+
+export default function TodoInput({ setTodoList }: Props) {
+  const [content, setContent] = React.useState<string>('');
   return (
     <Box>
-      <Input placeholder="할일 입력?" />
+      <Input
+        placeholder="할일 입력?"
+        value={content}
+        onChange={e => setContent(e.target.value)}
+        onKeyPress={e => {
+          if (content === '') return;
+          if (e.key !== 'Enter' && e.key !== 'NumpadEnter') return;
+          setTodoList({
+            id: '0',
+            content: content,
+            completed: false,
+            editing: false,
+          });
+          setContent('');
+        }}
+      />
     </Box>
   );
 }
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -42,6 +42,27 @@ export const TodoContent = styled.span<{ checked: boolean }>`
 `;
 
 export function HomePage() {
+  const [todoList, setTodoList] = React.useState<ITodoItem[]>([
+    {
+      id: '1',
+      content: '1 투두',
+      completed: true,
+      editing: false,
+    },
+    {
+      id: '2',
+      content: '2 투두',
+      completed: false,
+      editing: false,
+    },
+    {
+      id: '3',
+      content: '3 투두',
+      completed: false,
+      editing: false,
+    },
+  ]);
+
   return (
     <>
       <Helmet>
@@ -51,32 +72,13 @@ export function HomePage() {
       <Wrapper>
         <Box>
           <Title>할 일</Title>
-          <TodoInput />
+          <TodoInput
+            setTodoList={(todo: ITodoItem) => setTodoList([todo, ...todoList])}
+          />
           <TodoList>
-            <TodoItem
-              todo={{
-                id: '1',
-                completed: false,
-                content: '투두입니다 1',
-                editing: false,
-              }}
-            ></TodoItem>
-            <TodoItem
-              todo={{
-                id: '2',
-                completed: true,
-                content: '투두입니다 2',
-                editing: true,
-              }}
-            ></TodoItem>
-            <TodoItem
-              todo={{
-                id: '3',
-                completed: false,
-                content: '투두입니다 3',
-                editing: false,
-              }}
-            ></TodoItem>
+            {todoList.map(todo => (
+              <TodoItem todo={todo} />
+            ))}
           </TodoList>
         </Box>
       </Wrapper>
